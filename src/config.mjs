fde26de7aba5c40765d10cc6cf476fc24170133b
Ali Sa'ad Eddeen <ali.se@alameensoft.com>
2023-06-15 16:12:45 -0400

    modified some strings

--- src/config.mjs
+++ src/config.mjs
@@ -1,15 +1,15 @@
 import defaultImage from './assets/images/default.png';
 
 const CONFIG = {
-  name: 'AstroWind',
+  name: 'alameensoft',
 
   origin: 'https://astrowind.vercel.app',
   basePathname: '/',
   trailingSlash: false,
 
-  title: 'AstroWind — Free template for create a website with Astro + Tailwind CSS',
+  title: 'alameen Business Solutions Version 10.0 is here',
   description:
-    '🚀 Suitable for Startups, Small Business, Sass Websites, Professional Portfolios, Marketing Websites, Landing Pages & Blogs.',
+    'Revolutions are not made, they come. A new era of business solutions is here. alameen Business Solutions Version 10.0 is here.',
   defaultImage: defaultImage,
 
   defaultTheme: 'system', // Values: "system" | "light" | "dark" | "light:only" | "dark:only"
